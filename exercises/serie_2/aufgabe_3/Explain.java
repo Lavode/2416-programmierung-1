953a2d99d5b3e3204c99146870f958356000a50e
Michael Senn <michael@morrolan.ch>
2016-10-18 18:57:53 +0200

    Adds exercise 3.

---
+++ exercises/serie_2/aufgabe_3/Explain.java
@@ -0,0 +1,55 @@
+// Declare new class 'A'
+public class A {
+	// Declare instance variable 'a' as int, initialize to 3
+	private int a = 3;
+	// Declare method with no return value, which increments the value of a
+	public void increment() { a++; }
+	// Declare method, which returns the value of a as a string.
+	public String toString() { return "" + a; }
+}
+
+// Declare new class 'B'
+public class B {
+	// Declare class-level method 'main', which will serve as entry point if the compiled file is executed.
+	public static void main(String args[]) {
+		// Initialize new instance of A class, assign to 'a1'
+		A a1 = new A();
+		// Initialize new instance of A class, assign to 'a2'
+		A a2 = new A();
+		// Call 'increment' method on 'a1', thereby incrementing its 'a' instance variable.
+		a1.increment();
+		//
+		System.out.println(a1 + "/" + a2);
+		// Assign the object *referenced by* 'a1' to 'a2' - both a1 and a2 now reference the *same* object
+		a2 = a1;
+		// Call 'increment' method on 'a2', thereby incrementing its 'a' instance variable.
+		a2.increment();
+		//
+		System.out.println(a1 + "/" + a2);
+		// Initialize new instance of String class, initialize to "ROCK", assign to 's1'
+		String s1 = "ROCK";
+		// Assign *a copy* of the string referenced by 's1' to 's2', as strings are immutable.
+		String s2 = s1;
+		// Call toLowerCase method on s2, and assign result to s2
+		s2 = s2.toLowerCase();
+		//
+		System.out.println(s1 + "/" + s2);
+		// Declare j as int, initialize to 1
+		int j = 1;
+		// Declare i as int, initialize to *value* of j - they do *not* reference the same integer.
+		int i = j;
+		// Increment j - this does *not* increment i
+		j++;
+		//
+		System.out.println(j + "/" + i);
+	}
+}
+
+/* 
+ * This leads to the expected output of:
+ *
+ * 4/3
+ * 5/5
+ * ROCK/rock
+ * 2/1
+ */
