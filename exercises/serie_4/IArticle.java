8eb5faa1c3114ca4f941f33152427b5b2f2e2370
Michael Senn <michael@senn.email>
2016-11-17 07:28:47 +0100

    Adds second part of exercise.

---
+++ exercises/serie_4/IArticle.java
@@ -0,0 +1,10 @@
+public interface IArticle
+{
+
+	public int getId();
+
+	public int getPrice();
+
+	public String getDescription();
+
+}
---
+++ exercises/serie_4/Order.java
@@ -0,0 +1,72 @@
+//Lorenzo Wipfli 13-933-262
+//Michael Senn 
+
+import java.util.*;
+public class Order {
+	private static int currentId;
+
+
+	private int id;
+	private String customerName;
+	private String customerAddress;
+	ArrayList<IArticle> articles = new ArrayList<IArticle>();
+
+	public Order()
+	{
+		currentId++;
+		id = currentId;
+
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getCustomerName() {
+		return this.customerName;
+	}
+
+	public void setCustomerName(String customerName)
+	{
+		this.customerName=customerName;
+	}
+
+	public String getCustomerAddress() {
+		return this.customerAddress;
+	}
+
+	public void setCustomerAddress(String customerAddress)
+	{
+		this.customerAddress=customerAddress;
+	}
+
+	public Iterable<IArticle> getOrderedArticles() {
+		return this.articles;
+	}
+
+	public String toString()
+	{
+		String s = "Order id: "+id+", Customer: "+customerName+", "+ customerAddress+"\n";
+		for(int i = 0; i<articles.size();i++)
+		{
+			s=s+articles.get(i).toString()+"\n";
+		}
+		s = s+"Total price:  "+getTotalPrice()+" CHF \n";
+		return s;
+	}
+
+	public void add(IArticle article)
+	{
+		articles.add(article);
+	}
+
+	public int getTotalPrice()
+	{
+		int prize=0;
+		for(int i = 0; i<articles.size();i++)
+		{
+			prize=prize+articles.get(i).getPrice();
+		}
+		return prize;
+	}
+}
